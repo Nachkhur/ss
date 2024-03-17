63a36ce2a44cf4af529d463b3e7e5b9fb3b5a7da
Nachkhur <workingshmorking@gmail.com>
2024-03-17 13:55:42 +0300

    s

---
+++ SimpleCSharpConsoleApp/SimpleCSharpConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+using System.Dynamic;
+using System.Security;
+using System.Text;
+using static System.Formats.Asn1.AsnWriter;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] arr = {"Хирург", "Терапевт","Лор","Онколог","Кардиолог"};
+        Random rd = new Random();
+        for (int i = 0; i < 10; i++)
+        {
+            int rdIndex = rd.Next(arr.Length);
+            var rdValue = arr[rdIndex];
+            Console.WriteLine(rdValue);
+        }
+    }
+}
